f5a517c6fb7ae68ef144e557c82ce6f216893593
Mario Lazzari <mario.lazzari@gmail.com>
2022-08-27 12:00:07 +0200

    Done lesson 5

--- redux-demo/index.js
+++ redux-demo/index.js
@@ -1,1 +1,11 @@
-console.log("test");
+const CAKE_ORDERED = "CAKE_ORDERED";
+
+// action creator
+function orderCake() {
+  return {
+    type: CAKE_ORDERED,
+    quantity: 1,
+  };
+}
+
+// reducer
