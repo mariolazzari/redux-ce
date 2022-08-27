6d1a84bc5141873de8883010433da7d6e591f4ad
Mario Lazzari <mario.lazzari@gmail.com>
2022-08-27 12:34:49 +0200

    Done lesson 7

--- redux-demo/index.js
+++ redux-demo/index.js
@@ -1,3 +1,5 @@
+const { createStore } = require("redux");
+
 const CAKE_ORDERED = "CAKE_ORDERED";
 
 // action creator
@@ -8,4 +10,40 @@ function orderCake() {
   };
 }
 
+// initial state
+const initialState = {
+  cakes: 10,
+};
+
 // reducer
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CAKE_ORDERED:
+      return {
+        ...state,
+        cakes: state.cakes - action.quantity,
+      };
+
+    default:
+      return state;
+  }
+};
+
+// redux store
+const store = createStore(reducer);
+
+// state
+console.log("Initial state:", store.getState());
+
+// subscribe event
+const unsubscribe = store.subscribe(() => {
+  console.log("State updated:", store.getState());
+});
+
+// dispatch action
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+
+// unsubscribe
+unsubscribe();
